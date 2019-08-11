3746e9a6ec9031c731af6480dd678b1306cab7e0
mucheniski <mucheniski@gmail.com>
2019-08-11 11:51:47 -0300

    Secao6 aula48 - Domain de Categoria

--- src/main/java/com/mucheniski/resources/CategoriaResource.java
+++ src/main/java/com/mucheniski/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.mucheniski.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mucheniski.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST FUNCIONANDO!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(categoria1);
+		list.add(categoria2);
+		
+		return list;
+		
 	}
 	
 }
