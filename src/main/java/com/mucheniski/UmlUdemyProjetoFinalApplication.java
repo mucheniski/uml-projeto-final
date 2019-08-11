918932da9247ef614ab91aac9c5d0bd97865cfda
mucheniski <mucheniski@gmail.com>
2019-08-11 10:38:08 -0300

    Projeto criado

---
+++ src/main/java/com/mucheniski/UmlUdemyProjetoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.mucheniski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlUdemyProjetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlUdemyProjetoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mucheniski/UmlUdemyProjetoFinalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mucheniski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmlUdemyProjetoFinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
